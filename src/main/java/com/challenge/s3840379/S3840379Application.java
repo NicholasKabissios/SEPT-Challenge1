ae3ed96b3f4115f0fc481ff4918afb45a55cf02c
NicholasKabissios <s3840379@student.rmit.edu.au>
2022-09-09 14:35:07 +1000

    Initial commit

---
+++ src/main/java/com/challenge/s3840379/S3840379Application.java
@@ -0,0 +1,13 @@
+package com.challenge.s3840379;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3840379Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3840379Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenge/s3840379/S3840379ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.s3840379;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3840379ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
