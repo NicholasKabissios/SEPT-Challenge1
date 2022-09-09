c42bd6157c303db8bc0df238a59366810f289680
NicholasKabissios <s3840379@student.rmit.edu.au>
2022-09-09 15:33:28 +1000

    Finished ItemDao and worked on ItemController

--- src/main/java/com/challenge/s3840379/controller/ItemController.java
+++ src/main/java/com/challenge/s3840379/controller/ItemController.java
@@ -1,4 +1,38 @@
 package com.challenge.s3840379.controller;
 
+import com.challenge.s3840379.dao.ItemDao;
+import com.challenge.s3840379.model.Item;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class ItemController {
+
+    ItemDao dao = new ItemDao();
+
+    @PostMapping("/addItem")
+    public void addItem(@RequestBody Item newItem) {
+        dao.addItem(newItem);
+    }
+
+    @GetMapping("/getItem")
+    public String getItem(@RequestParam(value = "id", defaultValue = "asd") String id) {
+        return dao.getItem(id).toString();
+    }
+
+    @GetMapping("/getItems")
+    public String getItems() {
+        return dao.toString();
+    }
+
+    @DeleteMapping("/deleteItem")
+    public void deleteItem(@RequestParam(value = "id") String id) {
+        dao.deleteItem(id);
+    }
+
+    @PutMapping("/update")
+    public void updateItem(@RequestBody Item newItem) {
+        dao.updateItem(newItem);
+    }
+
 }
--- src/main/java/com/challenge/s3840379/dao/ItemDao.java
+++ src/main/java/com/challenge/s3840379/dao/ItemDao.java
@@ -1,4 +1,47 @@
 package com.challenge.s3840379.dao;
 
+import com.challenge.s3840379.model.Item;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
 public class ItemDao {
+
+    ArrayList<Item> items = new ArrayList<Item>();
+
+    public void addItem(Item item) {
+        items.add(item);
+    }
+
+    public void deleteItem(String id) {
+        for (int i = 0; i < items.size(); i++) {
+            if (items.get(i).getId().equals(id)) {
+                items.remove(i);
+                return;
+            }
+        }
+    }
+
+    public Item getItem(String id) {
+        for (int i = 0; i < items.size(); i++) {
+            if (items.get(i).getId().equals(id)) {
+                return items.get(i);
+            }
+        }
+        return null;
+    }
+
+    public void updateItem(Item item) {
+        for (int i = 0; i < items.size(); i++) {
+            if (items.get(i).getId().equals(item.getId())) {
+                items.set(i, item);
+            }
+        }
+    }
+
+    public String toString() {
+        return items.stream().map(Object::toString)
+                .collect(Collectors.joining("\n "));
+    }
+
 }
